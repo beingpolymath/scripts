3c0cacc41ca876e4b275daf96a226495dbb6a72b
saiteja <33036380+saiteja-pinnoju@users.noreply.github.com>
2023-02-17 23:33:18 +0530

    Update rapiddns.py

--- subdomains/rapiddns.py
+++ subdomains/rapiddns.py
@@ -1,32 +1,41 @@
+import argparse
 import requests
-import sys
 from bs4 import BeautifulSoup
 
-# Get the domain name from the command-line arguments
-try:
-    domain = sys.argv[1]
-except IndexError:
-    print("Please provide a domain name.")
-    exit()
 
-# Set the URL for fetching the subdomains
-subdomain_url = f"https://rapiddns.io/subdomain/{domain}?full=1"
+def main():
+    # Parse command-line arguments
+    parser = argparse.ArgumentParser(description='Fetch subdomains from RapidDNS API')
+    parser.add_argument('-d', '--domain', type=str, help='Target domain name', required=True)
+    parser.add_argument('-o', '--output', type=str, help='Output file for subdomains', default='subdomains.txt')
+    args = parser.parse_args()
 
-# Use requests to get the data from the URL
-response = requests.get(subdomain_url)
+    domain = args.domain
+    output_file = args.output
 
-# parse the HTML document using BeautifulSoup
-soup = BeautifulSoup(response.text, "html.parser")
+    # Set the URL for fetching the subdomains
+    subdomain_url = f"https://rapiddns.io/subdomain/{domain}?full=1"
 
-# open the subdomain_name in write mode and write the data to it
-subdomain_name=f"rapiddns-{domain}.txt"
-with open(subdomain_name, 'w') as out:
-    # find the <td> elements in the document
-    td_elements = soup.find_all("td")
+    # Use requests to get the data from the URL
+    response = requests.get(subdomain_url)
+
+    # parse the HTML document using BeautifulSoup
+    soup = BeautifulSoup(response.text, "html.parser")
+
+    # open the subdomain_name in write mode and write the data to it
+    subdomain_name = f"rapiddns-{domain}.txt"
+    with open(output_file, 'w') as out:
+        # find the <td> elements in the document
+        td_elements = soup.find_all("td")
+
+        # print the contents of the <td> elements that contain a domain name
+        for td in td_elements:
+            if td.string and "." in td.string:
+                subdomain = td.string.strip()
+                if subdomain:
+                    subdomain_text = f"{subdomain}"
+                    print(subdomain_text, file=out)
 
 
-# print the contents of the <td> elements that contain a domain name
-    for td in td_elements:
-        if td.string and "." in td.string:
-            print(td.string, file=out)
-
+if __name__ == '__main__':
+    main()
