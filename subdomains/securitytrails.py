e82843e6e414f04b12d3e6ba848d9c8d54794010
saiteja <33036380+saiteja-pinnoju@users.noreply.github.com>
2023-02-17 23:19:42 +0530

    Update securitytrails.py

--- subdomains/securitytrails.py
+++ subdomains/securitytrails.py
@@ -1,31 +1,35 @@
 import requests
 import json
-import sys
+import argparse
 
-# Get the domain name from the command-line arguments
-try:
-    domain = sys.argv[2]
-    api_key = sys.argv[4]
-except IndexError:
-    print("Please provide the -d domain name and -t API key as command-line arguments.")
-    exit()
+def parse_args():
+    # Parse command-line arguments
+    parser = argparse.ArgumentParser(description='Fetch subdomains from SecurityTrails API')
+    parser.add_argument('-d', '--domain', type=str, help='Target domain name', required=True)
+    parser.add_argument('-t', '--api_key', type=str, help='API key for SecurityTrails API', required=True)
+    parser.add_argument('-o', '--output', type=str, help='Output file for subdomains', default='subdomains.txt')
+    args = parser.parse_args()
 
+    domain = args.domain
+    api_key = args.api_key
+    output_file = args.output
 
-url = f"https://api.securitytrails.com/v1/domain/{domain}/subdomains"
-params = {"apikey": api_key}
+    url = f"https://api.securitytrails.com/v1/domain/{domain}/subdomains"
+    params = {"apikey": api_key}
 
-response = requests.get(url, params=params)
+    response = requests.get(url, params=params)
 
-if response.status_code == 200:
-    subdomains = response.json().get("subdomains", [])
+    if response.status_code == 200:
+        subdomains = response.json().get("subdomains", [])
+        with open(output_file, "w") as f:
+            for subdomain in subdomains:
+                subdomain_text = f"{subdomain}.{domain}\n"
+                f.write(subdomain_text)
 
-    subdomain_name=f"securitytrails-{domain}.txt"
-    with open(subdomain_name, "w") as f:
-        for subdomain in subdomains:
-            f.write(f"{subdomain}.{domain}\n")
-            print(f"{subdomain}.{domain}")
+        print(f"{len(subdomains)} subdomains written to {output_file}")
 
-else:
-    print(f"Error {response.status_code}: {response.text}")
+    else:
+        print(f"Error {response.status_code}: {response.text}")
 
-
+if __name__ == '__main__':
+    parse_args()
